3fd1189bf9536d41f6323da50b2ab8066a4e0b0a
Michael Fich <michaeltf@gmail.com>
2015-07-07 16:32:37 -0400

    Completed exercises variables.js and functions.js

--- functions.js
+++ functions.js
@@ -8,6 +8,13 @@
 // takes 4 arguments: number of children, partner's name, geographic location, job title.
 // outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
 // Call that function 3 times with 3 different values for the arguments.
+function tellFortune(children, partner, place, job) {
+  document.write("<p>You will be a " + job + " in " + place +
+    ", and married to " + partner + " with " + children + " kids.</p>");
+}
+tellFortune(6, "Barbara Bush", "Texas", "Cowboy");
+tellFortune(3, "Beyonce", "New York", "Janitor");
+tellFortune(4, "Helen Keller", "Zimbabwe", "President")
 
 // EXERCISE: The Age Calculator
 
@@ -17,8 +24,20 @@
 // takes 2 arguments: birth year, current year.
 // calculates the 2 possible ages based on those years.
 // outputs the result to the screen like so: "You are either NN or NN"
+function calculateAge(birthYear, currentYear) {
+  currentYear = currentYear || new Date().getFullYear();
+  var ages = [
+    currentYear - birthYear,
+    currentYear - birthYear - 1
+  ];
+  document.write("<p>They are either " + ages[0] +" or " + ages[1] + "</p>");
+}
 // Call the function three times with different sets of values.
 // Bonus: Figure out how to get the current year in JavaScript instead of passing it in.
+calculateAge(1984, 2020);
+calculateAge(1984);
+calculateAge(2000, 2020);
+calculateAge(2000);
 
 // EXERCISE: The Lifetime Supply Calculator
 
@@ -28,32 +47,72 @@
 // takes 2 arguments: age, amount per day.
 // calculates the amount consumed for rest of the life (based on a constant max age).
 // outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
+function calculateSupply(age, quantity) {
+  var maxAge = 75;
+  var total = (maxAge - age) * (quantity * 365);
+  console.log(total);
+  document.write("<p>You will need " + Math.round(total) +
+    " to last you until the ripe old age of " + maxAge + "</p>");
+}
 // Call that function three times, passing in different values each time.
 // Bonus: Accept floating point values for amount per day, and round the result to a round number.
+calculateSupply(31, 1.23);
+calculateSupply(45, 8.97);
+calculateSupply(18, 2.1);
 
 // EXERCISE: The Geometrizer
 
 // Create 2 functions that calculate properties of a circle, using the definitions here.
 
 // Create a function called calcCircumfrence:
-
 // Pass the radius to the function.
 // Calculate the circumference based on the radius, and output "The circumference is NN".
-// Create a function called calcArea:
+function calcCircumfrence(radius) {
+  var circumference = 2 * Math.PI * radius;
+  document.write("<p>The circumference is " + circumference + "</p>");
+}
 
+calcCircumfrence(25);
+calcCircumfrence(32);
+calcCircumfrence(45.5);
+
+// Create a function called calcArea:
 // Pass the radius to the function.
 // Calculate the area based on the radius, and output "The area is NN".
+function calcArea(radius) {
+  var area = Math.PI * Math.pow(radius, 2);
+  document.write("<p>The area is " + area + "</p>");
+}
+
+calcArea(25);
+calcArea(32);
+calcArea(45.5);
 
 // EXERCISE: The Temperature Converter
 
 // It's hot out! Let's make a converter based on the steps here.
 
 // Create a function called celsiusToFahrenheit:
-
 // Store a celsius temperature into a variable.
 // Convert it to fahrenheit and output "NN°C is NN°F".
-// Create a function called fahrenheitToCelsius:
+function celsiusToFahrenheit(celsius) {
+  var fahrenheit = celsius * 9 / 5 + 32;
+  document.write("<p>" + celsius + "&deg;C is " + fahrenheit + "&deg;F</p>");
+}
 
+celsiusToFahrenheit(0);
+celsiusToFahrenheit(32);
+celsiusToFahrenheit(65);
+
+// Create a function called fahrenheitToCelsius:
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
 
+function fahrenheitToCelsius(fahrenheit) {
+  var celsius = (fahrenheit - 32) * 5 / 9;
+  document.write("<p>" + fahrenheit + "°F is " + celsius + "°C.</p>");
+}
+
+fahrenheitToCelsius(32);
+fahrenheitToCelsius(65);
+fahrenheitToCelsius(100);
--- variables.js
+++ variables.js
@@ -5,42 +5,69 @@
 // Why pay a fortune teller when you can just program your fortune yourself?
 
 // Store the following into variables: number of children, partner's name, geographic location, job title.
+var numberOfChildren = 15.3;
+var partnersName = "Betty White";
+var place = "Deserted Island";
+var jobTitle = "Captain";
 // Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+document.write("<p>You will be a " + jobTitle + " in " +
+  place + ", and married to " + partnersName + " with " +
+  numberOfChildren + " kids.</p>");
 
 // EXERCISE: The Age Calculator
 
 // Forgot how old someone is? Calculate it!
 
 // Store the current year in a variable.
+var currentYear = new Date().getFullYear();
 // Store their birth year in a variable.
+var birthYear = 1984;
 // Calculate their 2 possible ages based on the stored values.
+var topAge = currentYear - birthYear;
+var bottomAge = topAge - 1;
 // Output them to the screen like so: "They are either NN or NN", substituting the values.
+document.write("<p>They are either " + topAge +" or " + bottomAge + "</p>");
 
 // EXERCISE: The Lifetime Supply Calculator
 
 // Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
 
 // Store your current age into a variable.
+var myCurrentAge = 31;
 // Store a maximum age into a variable.
+var maxAge = 62;
 // Store an estimated amount per day (as a number).
+var daylyQuantity = 5;
 // Calculate how many you would eat total for the rest of your life.
+var lifetimeQuantity = (maxAge - myCurrentAge) * (daylyQuantity * 365)
 // Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
+document.write("<p>You will need " + lifetimeQuantity +
+  " to last you until the ripe old age of " + maxAge + "</p>");
 
 // EXERCISE: The Geometrizer
 
 // Calculate properties of a circle, using the definitions here.
 
 // Store a radius into a variable.
+var radius = 20;
 // Calculate the circumference based on the radius, and output "The circumference is NN".
+var circumference = 2 * Math.PI * radius;
+document.write("<p>The circumference is " + circumference + "</p>")
 // Calculate the area based on the radius, and output "The area is NN".
+var area = Math.PI * Math.pow(radius, 2);
+document.write("<p>The area is " + area + "</p>");
 
 // EXERCISE: The Temperature Converter
 
 // It's hot out! Let's make a converter based on the steps here.
 
 // Store a celsius temperature into a variable.
+var tempInC = 32;
 // Convert it to fahrenheit and output "NN°C is NN°F".
+var convertToF = tempInC * 9 / 5 + 32;
+document.write("<p>" + tempInC + "&deg;C is " + convertToF + "&deg;F</p>");
 // Now store a fahrenheit temperature into a variable.
+var tempInF = 81;
 // Convert it to celsius and output "NN°F is NN°C."
-
-
+var convertToC = (tempInF - 32) * 5 / 9;
+document.write("<p>" + tempInF + "°F is " + convertToC + "°C.</p>");
